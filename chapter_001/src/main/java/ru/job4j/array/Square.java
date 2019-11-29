4b26df16471a9b99192942037ca6a2aec624cfc2
AndreyShulgin <neonod404@gmail.com>
2019-11-29 22:24:13 +0300

    Заполнить массив степенями чисел.

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * Класс Square
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ * @version 1.0
+ */
+public class Square {
+
+    /**
+     * Метод возводит числа от 1 до bound и добавляет их в массив.
+     * @param bound - bound.
+     * @return - массив квадратов от 1 до bound.
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int index = 1; index <= bound; index++) {
+            rst [index - 1] = index * index;
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Пакет содержит задания по массивам
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ * @version 1
+ * @since 1
+ */
+package ru.job4j.array;
