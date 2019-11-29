6b1aab7898c73ec5f8728236a9ba7f3e333565bf
AndreyShulgin <neonod404@gmail.com>
2019-11-29 18:50:46 +0300

    JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,27 +1,51 @@
 package ru.job4j.calculator;
 
+/**Calculator
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ * @version 1.0
+ */
 public class Calculator {
 
+    /**
+     * Сложение first и second.
+     * @param first - первое число.
+     * @param second - второе число.
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /** Деление first на second.
+     * @param first - первое число.
+     * @param second - второе число.
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
 
+    /** Умножение first и second.
+     * @param first - первое число.
+     * @param second - второе число.
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
 
+    /** Вычитание из first second.
+     * @param first - первое число.
+     * @param second - второе число.
+     */
     public static void subtract(double first, double second) {
         double result =  first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /** Метод main для запуска.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Пакет содержит классы Calculator и Fit
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ * @version 1
+ * @since 1
+ */
+package ru.job4j.calculator;
