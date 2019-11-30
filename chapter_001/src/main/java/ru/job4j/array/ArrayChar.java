23df4d4bc1d9be22309941427e309819155dd2fc
AndreyShulgin <neonod404@gmail.com>
2019-11-30 12:23:29 +0300

    Слово начинается с ...

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * ArrayChar
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ * @version 1.0
+ */
+public class ArrayChar {
+
+    /**
+     * Массив проверяет совпадают ли начало массива word с массивом pref
+     * @param word - массив
+     * @param pref - массив
+     * @return - true если совпадают
+     */
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int index = 0; index < pref.length; index++) {
+            if (word[index] != pref[index]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
