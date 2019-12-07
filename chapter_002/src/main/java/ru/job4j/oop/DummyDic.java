40d4c1e52fa0be16a8fa5c6f4e08dd03114aa7f2
AndreyShulgin <neonod404@gmail.com>
2019-12-07 17:50:56 +0300

    Вызов метод с возвращаемым типом.

---
+++ chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class DummyDic {
+
+    /**
+     * Метод возвращает заданную строку
+     * @param eng текст.
+     * @return текст.
+     */
+    public String engToRus(String eng) {
+        return "Неизвестное слово. " + eng;
+    }
+}
