87f183379c5d9863cbeecdfd843d22f19cfc8734
AndreyShulgin <neonod404@gmail.com>
2019-12-07 17:27:15 +0300

    Создание объекта

---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class Dog {
+    /**
+     * Метод для запаска
+     * @param args - args
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
