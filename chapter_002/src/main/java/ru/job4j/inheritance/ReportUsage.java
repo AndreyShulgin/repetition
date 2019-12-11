30beb59457bec6e3fb78cb4a455afe904c7f8dde
AndreyShulgin <neonod404@gmail.com>
2019-12-11 12:05:15 +0300

    Аннотация @Override

--- chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -4,6 +4,7 @@ package ru.job4j.inheritance;
  * @author Andrey Shulgin (neonod404@gmail.com)
  */
 public class ReportUsage {
+
     public static void main(String[] args) {
         JSONReport report = new JSONReport();
         String text = report.generate("Report's name", "Report's body");
--- chapter_002/src/main/java/ru/job4j/inheritance/TextReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/TextReport.java
@@ -4,6 +4,7 @@ package ru.job4j.inheritance;
  * @author Andrey Shulgin (neonod404@gmail.com)
  */
 public class TextReport {
+
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
