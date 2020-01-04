a3d740343feb7af6a6ccaa3d9f85b7eb1dee468d
AndreyShulgin <neonod404@gmail.com>
2020-01-04 13:28:47 +0300

    Организовать сортировку User

---
+++ CollectionsLite/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(@NotNull User o) {
+        int result = this.name.compareTo(o.name);
+        if (result == 0) {
+            result = Integer.compare(this.age, o.age);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
