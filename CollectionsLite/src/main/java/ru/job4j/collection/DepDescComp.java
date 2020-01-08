e79a2cf5bbc3b15cbcbd418be7b6b38529d8b9e0
AndreyShulgin <neonod404@gmail.com>
2020-01-09 01:45:03 +0300

    Отсортировать департаменты

--- CollectionsLite/src/main/java/ru/job4j/collection/DepDescComp.java
+++ CollectionsLite/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,14 +8,14 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int result = o2.compareTo(o1);
+        int result = o2.length() - o1.length();
         int length = Math.min(o1.length(), o2.length());
         for (int index = 0; index < length; index++) {
-            result = Character.compare(o2.charAt(index), o1.charAt(index));
-            if (result != 0) {
+            if (o2.charAt(index) != o1.charAt(index)) {
                 result = o1.compareTo(o2);
                 break;
             }
+            result = o2.length() - o1.length();
         }
         return result;
     }
--- CollectionsLite/src/test/java/ru/job4j/collection/DepDescCompTest.java
+++ CollectionsLite/src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -11,7 +11,7 @@ public class DepDescCompTest {
     @Test
     public void compare() {
         int rsl = new DepDescComp().compare(
-                "K2/SK1/SSK2",
+                "K2",
                 "K2/SK1/SSK1"
         );
         assertThat(rsl, greaterThan(0));
