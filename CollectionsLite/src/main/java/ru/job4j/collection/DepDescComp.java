a4dfd08cbef852a14f7cbaa18e6cf2bdb8bdee9b
AndreyShulgin <neonod404@gmail.com>
2020-01-09 00:05:47 +0300

    Отсортировать департаменты

--- CollectionsLite/src/main/java/ru/job4j/collection/DepDescComp.java
+++ CollectionsLite/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,10 +8,8 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String o1part = o1.substring(0, 1);
-        String o2part = o2.substring(0, 1);
         int result;
-        if (o1part.compareTo(o2part) == 0) {
+        if (o1.charAt(1) == (o2.charAt(1))) {
             result = o1.compareTo(o2);
         } else {
             result = o2.compareTo(o1);
