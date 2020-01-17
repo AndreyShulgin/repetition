f8c19f69385cdc8b2b040317d323e46c996cdf18
AndreyShulgin <neonod404@gmail.com>
2020-01-17 12:39:11 +0300

    Создать convert(Iterator<Iterator>)

---
+++ CollectionsPro/src/main/java/ru/job4j/iterator/Converter.java
@@ -0,0 +1,32 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class Converter {
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<>() {
+            Iterator<Integer> iterator = it.next();
+
+            @Override
+            public boolean hasNext() {
+                while (it.hasNext() && !iterator.hasNext()) {
+                    iterator = it.next();
+                }
+                return iterator.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (!this.hasNext()) {
+                    throw new NoSuchElementException("No elements");
+                }
+                return iterator.next();
+            }
+        };
+    }
+}
